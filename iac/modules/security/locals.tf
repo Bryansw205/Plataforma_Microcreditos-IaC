9627e37a39a8ec327c459e372a586d5af90dd402
angelopohl <angelfrancasanovach@gmail.com>
2026-06-10 21:22:58 -0500

    chore(security): Añadir configuracion base modulo

--- iac/modules/security/locals.tf
+++ iac/modules/security/locals.tf
@@ -1,1 +1,13 @@
-# Locals
+locals {
+  name_prefix = "${var.project_name}-${var.environment}"
+
+  common_tags = merge(
+    {
+      Project     = var.project_name
+      Environment = var.environment
+      Layer       = "security"
+      ManagedBy   = "terraform"
+    },
+    var.tags
+  )
+}
