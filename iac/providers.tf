535941eb170038ed3b6165c9fb45450af1cb52a4
joaquinpro <joaquinsanchezma4@gmail.com>
2026-06-24 17:24:03 -0500

    feat(base): agregar provider aliases para modulos con configuracion regional

--- iac/providers.tf
+++ iac/providers.tf
@@ -5,3 +5,23 @@ provider "aws" {
     tags = local.common_tags
   }
 }
+
+# Alias requerido por los módulos networking y security (WAF global)
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = local.common_tags
+  }
+}
+
+# Alias requerido por los módulos auth y config
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = local.common_tags
+  }
+}
