a07e9cb7aba52be88671fb4f0a42e3ead0d70692
joaquinpro <joaquinsanchezma4@gmail.com>
2026-06-24 17:25:13 -0500

    fix(auth): eliminar tags no soportados en aws_cognito_user_group

--- iac/modules/auth/user_groups.tf
+++ iac/modules/auth/user_groups.tf
@@ -2,18 +2,10 @@ resource "aws_cognito_user_group" "admin" {
   name         = "admin"
   user_pool_id = aws_cognito_user_pool.main.id
   description  = "Administradores con acceso total a la aplicación"
-
-  tags = merge(local.common_tags, {
-    Name = "${local.name_prefix}-admin-group"
-  })
 }
 
 resource "aws_cognito_user_group" "user" {
   name         = "user"
   user_pool_id = aws_cognito_user_pool.main.id
   description  = "Usuarios regulares con acceso limitado a la aplicación"
-
-  tags = merge(local.common_tags, {
-    Name = "${local.name_prefix}-user-group"
-  })
 }
