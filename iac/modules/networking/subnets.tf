9e5306129163906c81937b115142bb7d527f3b6b
YamileCenta <ycentaf1@upao.edu.pe>
2026-06-25 17:36:57 -0500

    fix(networking): Cambiar asignación de IP publica

--- iac/modules/networking/subnets.tf
+++ iac/modules/networking/subnets.tf
@@ -1,13 +1,18 @@
-# Public Subnets
+# ============================================================
+# MODULE: networking (subnets.tf)
+# Definición de subredes públicas y privadas (Multi-AZ)
+# ============================================================
 
-# checkov:skip=CKV_AWS_130: Se requiere la asignacion automatica de IP publica de forma intencional por tratarse de la capa publica de la red. :3
+# Public Subnets
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
 
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+  # SOLUCIÓN CKV_AWS_130: Desactivar asignación automática por defecto.
+  # Protege la red impidiendo que recursos mal configurados expongan su IP a internet de nacimiento.
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.project_name}-${var.environment}-public-${var.availability_zones[count.index]}"
