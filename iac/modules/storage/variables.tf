e4e9a63cd635155a27f6223ef2dc573b72126fd4
Maryory <maryory.vale.5@gmail.com>
2026-06-25 18:18:07 -0500

    fix(iac): resolve undeclared variables in storage and cdn modules from dev update

--- iac/modules/storage/variables.tf
+++ iac/modules/storage/variables.tf
@@ -12,3 +12,21 @@ variable "s3_kms_key_arn" {
   description = "ARN de la llave KMS para el cifrado seguro de documentos en reposo"
   type        = string
 }
+
+variable "log_bucket_domain_name" {
+  description = "Domain of the log bucket"
+  type        = string
+  default     = ""
+}
+
+variable "replication_iam_role_arn" {
+  description = "ARN of the replication IAM role"
+  type        = string
+  default     = ""
+}
+
+variable "sns_topic_arn" {
+  description = "ARN of SNS topic for bucket notifications"
+  type        = string
+  default     = ""
+}
