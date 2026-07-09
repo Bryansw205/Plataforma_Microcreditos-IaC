15e4b4f9af89275e61d8c6396905c8e525bc8d1d
Bryan Mariños <bryanmg.020@gmail.com>
2026-07-08 20:39:27 -0500

    feat(IaC_): Añadir servicio ElastiCache

---
+++ IaC_/elasticache.tf
@@ -0,0 +1,33 @@
+resource "aws_elasticache_subnet_group" "redis" {
+  name       = "${local.name_prefix}-redis-subnet-group"
+  subnet_ids = aws_subnet.private_data[*].id
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-redis-subnet-group"
+  })
+}
+
+resource "aws_elasticache_replication_group" "redis" {
+  replication_group_id       = "${local.name_prefix}-redis"
+  description                = "ElastiCache Redis para ${local.name_prefix}"
+  engine                     = "redis"
+  engine_version             = "7.0"
+  node_type                  = "cache.t3.micro"
+  port                       = var.redis_port
+  parameter_group_name       = "default.redis7"
+  subnet_group_name          = aws_elasticache_subnet_group.redis.name
+  security_group_ids         = [aws_security_group.redis.id]
+  automatic_failover_enabled = true
+  multi_az_enabled           = true
+  num_cache_clusters         = 2
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+  apply_immediately          = true
+  maintenance_window         = "sun:05:00-sun:06:00"
+  snapshot_window            = "03:00-04:00"
+  snapshot_retention_limit   = 7
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-redis"
+  })
+}
