16a3bc49ee4e35256ea6f0c57536808e50b9dea1
Maryory <maryory.vale.5@gmail.com>
2026-07-09 07:29:32 -0500

    feat: corregir la definicion de roles de IAM y políticas personalizadas

--- IaC_/iam.tf
+++ IaC_/iam.tf
@@ -1,8 +1,6 @@
-# ============================================================
-# AWS IAM - Roles y Politicas para ECS Fargate
-# ============================================================
 
-# 1. ECS Task Execution Role (Permite a Fargate descargar imagenes y leer secretos/parametros)
+# AWS IAM - Roles y Politicas para ECS Fargate
+
 resource "aws_iam_role" "ecs_execution_role" {
   name = "${local.name_prefix}-ecs-execution-role"
 
@@ -24,13 +22,11 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
-# Adjuntar politica estandar de ejecucion de tareas ECS de AWS
 resource "aws_iam_role_policy_attachment" "ecs_execution_standard" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Politica personalizada para lectura de Secrets Manager y SSM Parameter Store con descifrado KMS
 resource "aws_iam_policy" "ecs_execution_secrets" {
   name        = "${local.name_prefix}-ecs-execution-secrets-policy"
   description = "Permite a ECS Execution Role acceder a Secretos y Parametros cifrados con KMS"
@@ -44,8 +40,7 @@ resource "aws_iam_policy" "ecs_execution_secrets" {
           "secretsmanager:GetSecretValue"
         ]
         Resource = [
-          aws_secretsmanager_secret.db_api_credenciales.arn,
-          aws_rds_cluster.aurora.master_user_secret[0].secret_arn
+          aws_secretsmanager_secret.db_api_credenciales.arn
         ]
       },
       {
@@ -77,8 +72,6 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_secrets_attach" {
   policy_arn = aws_iam_policy.ecs_execution_secrets.arn
 }
 
-
-# 2. ECS Task Role (Permite al contenedor Backend/Worker consumir recursos de AWS)
 resource "aws_iam_role" "ecs_task_role" {
   name = "${local.name_prefix}-ecs-task-role"
 
@@ -100,7 +93,6 @@ resource "aws_iam_role" "ecs_task_role" {
   })
 }
 
-# Politica de accesos para la aplicacion (S3, SQS y KMS para mensajeria/archivos)
 resource "aws_iam_policy" "ecs_task_app_policy" {
   name        = "${local.name_prefix}-ecs-task-app-policy"
   description = "Permite a las tareas de ECS interactuar con S3, SQS y descifrar con KMS"
