9b7fb38b6a9c121603cde0d0ce01e24973b38ade
Yamile Centa <yamilecenta0519@gmail.com>
2026-07-15 01:49:16 -0500

    fix: Integracion del sns topic para alertas de seguridad (CKV_AWS_252)

--- IaC_/cloudtrail.tf
+++ IaC_/cloudtrail.tf
@@ -7,6 +7,7 @@ resource "aws_cloudtrail" "main" {
   enable_log_file_validation    = true
 
   kms_key_id = aws_kms_key.main.arn
+  sns_topic_name = aws_sns_topic.alerts.arn
 
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail.arn
