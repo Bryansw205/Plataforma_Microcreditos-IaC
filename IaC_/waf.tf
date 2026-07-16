7214a7eef1c0dd4c21cba3c68055ae6c7658344f
Bryan Mariños <bryanmg.020@gmail.com>
2026-07-15 19:58:20 -0500

    fix: Corregir formato de elasticache.tf y waf.tf

--- IaC_/waf.tf
+++ IaC_/waf.tf
@@ -1,5 +1,5 @@
 resource "aws_wafv2_web_acl" "main" {
-# checkov:skip=CKV2_AWS_31: El logging de WAF se gestiona mediante auditoria centralizada en el bucket de logs de S3
+  # checkov:skip=CKV2_AWS_31: El logging de WAF se gestiona mediante auditoria centralizada en el bucket de logs de S3
   provider    = aws.us_east_1
   name        = "${local.name_prefix}-waf"
   description = "Web ACL para proteger el ALB de ${local.name_prefix}"
