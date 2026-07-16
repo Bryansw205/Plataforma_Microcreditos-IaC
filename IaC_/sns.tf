d79b59e6520df4bcc715dd68677758ff9a388ad4
Yamile Centa <yamilecenta0519@gmail.com>
2026-07-15 20:27:28 -0500

    fix: Corregir politicas

--- IaC_/sns.tf
+++ IaC_/sns.tf
@@ -1,12 +1,54 @@
 resource "aws_sns_topic" "alerts" {
   name              = "${local.name_prefix}-alerts"
-  kms_master_key_id = "alias/aws/sns"
+  kms_master_key_id = aws_kms_key.main.arn
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-alerts-topic"
     Type = "alerts"
   })
 }
+
+resource "aws_sns_topic_policy" "alerts" {
+  arn = aws_sns_topic.alerts.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudTrailToPublish"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "SNS:Publish"
+        Resource = aws_sns_topic.alerts.arn
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      },
+      {
+        Sid    = "AllowS3ToPublish"
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action   = "SNS:Publish"
+        Resource = aws_sns_topic.alerts.arn
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn" = [
+              aws_s3_bucket.documents.arn,
+              aws_s3_bucket.audit.arn
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
 
 resource "aws_sns_topic_subscription" "alerts_email" {
   count = var.alert_email != "" ? 1 : 0
